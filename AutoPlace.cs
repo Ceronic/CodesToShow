aac726a0529d56af23e76e27f5780239d19775ac
Ceronic <63244682+Ceronic@users.noreply.github.com>
2021-10-03 00:29:41 +0200

    Create AutoPlace.cs

---
+++ AutoPlace.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoPlace : MonoBehaviour
+{
+    public Transform objectPlace;
+    public GameObject theObject;
+    public GameObject theObject2;
+    public bool itemInPlace;
+
+    private Sfx _sfx;
+
+    private PlayerController _playerController;
+
+    private void Awake()
+    {
+        _sfx = GetComponent<Sfx>();
+        _playerController = FindObjectOfType<PlayerController>(); // To Fix Sound Bug in AutoPlace Script... patchworks *Rob*
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == theObject && itemInPlace == false)
+        {
+            SetPlaceObject(theObject);
+            //Added SFX On Slotted In Object
+            _sfx.PlayRandomSfx();
+        }
+
+        if (other.gameObject == theObject2 && itemInPlace == false)
+        {
+            SetPlaceObject(theObject2);
+            //Added SFX On Slotted In Object
+            _sfx.PlayRandomSfx();
+        }
+
+    }
+    
+    public void SetPlaceObject(GameObject thisObject)
+    {
+        thisObject.transform.position = objectPlace.transform.position;
+        thisObject.GetComponent<InteractableObject>().isInteracted = false;
+        thisObject.GetComponent<Rigidbody>().useGravity = false;
+        thisObject.GetComponent<InteractableObject>().enabled = false;
+        thisObject.GetComponent<InteractableObject>().isCanBePickedUp = false;
+
+        thisObject.GetComponent<LookingAtIT>().SetEnabled(false);
+
+        thisObject.transform.parent = this.transform;
+        thisObject.transform.localRotation = Quaternion.Euler(0, 0, 0);
+        itemInPlace = true;
+        _playerController.objectInHand = null;
+    }
+}
